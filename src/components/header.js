7cfdb6ceeaaf6e01a3655eb181c10615339e2f0c
nirmalyamitra <niramlya27.mitra@gmail.com>
2018-06-17 14:56:15 +0530

    1.1

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,11 @@ const getYear = () =>{
     const newDate = new Date();
     return newDate.getFullYear();
 }
+const user = () =>{
+    firstname= 'Nirmalya',
+    lastname= 'Mitra',
+    age= '27'
+}
 
 const Header = () => {
     return(
